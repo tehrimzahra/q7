36651fdec36035becbb9ff596b4ae94a599d60f4
tehrimzahra <tehrimzahra984@gmail.com>
2024-08-03 15:33:34 +0500

    first commit

---
+++ class7.py
@@ -0,0 +1,10 @@
+
+s = "   Python is fun!   "
+trimmed = s.strip()
+left_justified = trimmed.ljust(20, '*')
+
+
+right_justified = trimmed.rjust(20, '*')
+
+
+trimmed, left_justified, right_justified
